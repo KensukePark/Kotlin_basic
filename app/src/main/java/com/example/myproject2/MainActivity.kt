bdfd16c543e99042617bad76198f5362fdddfa2d
KensukePark <iu4148@gmail.com>
2023-10-29 02:14:40 +0900

    Exercise

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.myproject2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
+        //위의 binding부터 setContentView까지의 세줄로 view binding 하는 코드. binding.btn~~ 로 사용 가능
         MoveToEdit();
     }
 
@@ -23,5 +29,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, FlagmentActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
--- app/src/main/java/com/example/myproject2/TextEditActivity.kt
+++ app/src/main/java/com/example/myproject2/TextEditActivity.kt
@@ -1,9 +1,15 @@
 package com.example.myproject2
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class TextEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +26,47 @@ class TextEditActivity : AppCompatActivity() {
         }
         val btn_login = findViewById<Button>(R.id.btn_login)
         btn_login.setOnClickListener {
+            val Edit_login_id = findViewById<EditText>(R.id.login_id)
+            var id = Edit_login_id.text.toString()
+
+            val Edit_login_pw = findViewById<EditText>(R.id.login_password)
+            var pw = Edit_login_pw.text.toString()
+
+            val prefs = getSharedPreferences("file name", Context.MODE_PRIVATE)
+            val id_prefs = prefs.getString("id", "")
+            val pw_prefs = prefs.getString("pw", "")
+
+            if(id == id_prefs && pw == pw_prefs){
+                // 로그인 성공 다이얼로그 보여주기
+                dialog("success")
+            }
+            else{
+                // 로그인 실패 다이얼로그 보여주기
+                dialog("fail")
+            }
 
         }
+    }
+    fun dialog(type: String){
+        var dialog = AlertDialog.Builder(this)
+        if(type.equals("success")){
+            dialog.setTitle("로그인 성공")
+            dialog.setMessage("로그인 성공!")
+        }
+        else if(type.equals("fail")){
+            dialog.setTitle("로그인 실패")
+            dialog.setMessage("아이디와 비밀번호를 확인해주세요")
+        }
+
+        var dialog_listener = object: DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when(which){
+                    DialogInterface.BUTTON_POSITIVE ->
+                        Log.d(TAG, "")
+                }
+            }
+        }
+        dialog.setPositiveButton("확인",dialog_listener)
+        dialog.show()
     }
 }
