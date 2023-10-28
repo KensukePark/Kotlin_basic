b6523cea83a4f064aacd5a24c22505f73a8b5e23
KensukePark <iu4148@gmail.com>
2023-10-29 01:11:46 +0900

    Exercise

--- app/src/main/java/com/example/myproject2/TextEditActivity.kt
+++ app/src/main/java/com/example/myproject2/TextEditActivity.kt
@@ -2,10 +2,22 @@ package com.example.myproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TextEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_edit)
+    }
+
+    fun BtnClick() {
+        val btn_regist = findViewById<Button>(R.id.btn_regist)
+        btn_regist.setOnClickListener {
+
+        }
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        btn_login.setOnClickListener {
+
+        }
     }
 }
