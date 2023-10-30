8e4d84b45165948e9b1c90c2cac5d3477f8533ef
KensukePark <iu4148@gmail.com>
2023-10-31 03:53:04 +0900

    Exercise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,18 @@ android {
 }
 
 dependencies {
+    //gson
+    implementation ("com.google.code.gson:gson:2.8.6")
+
+
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.9.0")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:3.4.1")
+
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
