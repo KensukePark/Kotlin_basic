b91e01c5ce8fa4d38a9a90dff866cc94941bed5a
KensukePark <iu4148@gmail.com>
2023-11-05 23:50:02 +0900

    Exercise

---
+++ app/src/main/java/com/example/myproject2/AddingActivit.kt
@@ -0,0 +1,11 @@
+package com.example.myproject2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddingActivit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adding)
+    }
+}
